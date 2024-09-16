f2b930d75b174919cad51c6ee08b392af29e31e9
DVMath <dvillela@gmail.com>
2024-09-16 11:14:37 -0300

    index problem

--- src/signow/data_generator_daniel.py
+++ src/signow/data_generator_daniel.py
@@ -323,9 +323,12 @@ def generate_file_target(
     print("dfr")
     print(dfr.iloc[:, iind_target])
     #iind_target = 28-1
-    #target_df["value"] = dfr.iloc[:, iind_target]
+    target_df["value"] = dfr.iloc[:, iind_target]
     
-    target_df = dfr.iloc[:, iind_target] # target_df.loc[index, "value"]
+    print("target_df")
+    print(target_df)
+    #target_df = dfr.iloc[:, iind_target] # target_df.loc[index, "value"]
+    target_df = target_df.loc[index, "value"]
     target_df.index.name = "ref_date"
     target_df.index = pd.to_datetime(target_df.index, dayfirst=True)
 
